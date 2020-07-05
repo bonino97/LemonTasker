70ce3c84384fa8a297151379326525d45676c3f1
Juan Cruz <juanbonino97@gmail.com>
2020-07-04 21:13:52 -0300

    Add new proyect + express-validator

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+//Importar Express-Validator
+//https://express-validator.github.io/docs/
+const { body } = require('express-validator');
+
 //Importar Controlador
 const ProyectosController = require('../Controllers/ProyectosController');
+const Proyectos = require('../Models/Proyectos');
 
 
 
@@ -13,9 +18,15 @@ module.exports = ()=> {
     //GET
     router.get('/', ProyectosController.Home);
     router.get('/nuevo-proyecto', ProyectosController.FormularioProyecto);
+    // Listar Proyecto
+    router.get('/proyectos/:url', ProyectosController.ProyectoPorUrl);
 
     //POST
-    router.post('/nuevo-proyecto', ProyectosController.NuevoProyecto)
+    router.post(
+        '/nuevo-proyecto', 
+        body('nombre').not().isEmpty().trim().escape(),
+        ProyectosController.NuevoProyecto
+    );
 
     return router;
 }
