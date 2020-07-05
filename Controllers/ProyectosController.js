3012497ee0fb5b0b3860be6b4185374daae77db6
Juan Cruz <juanbonino97@gmail.com>
2020-07-04 21:14:15 -0300

    Update proyectosController

--- Controllers/ProyectosController.js
+++ Controllers/ProyectosController.js
@@ -1,34 +1,78 @@
-exports.Home = (req,res) => {
-    res.render('Index', { // Llama al View llamado 'Index'
-        nombrePagina: 'Proyectos'
-    });
-}
+const Proyectos = require('../Models/Proyectos');
 
-exports.FormularioProyecto = (req,res) => {
-    res.render('NuevoProyecto', { 
-        nombrePagina: 'Nuevo Proyecto'
-    });
-}
-
-exports.NuevoProyecto = (req,res) => {
-
-    //Validar que tengamos algo en el input.
-    const { nombre } = req.body;
+exports.Home = async (req,res) => {
     
-    let errores = [];
+    const proyectos = await Proyectos.findAll();
 
-    if(!nombre){
-        errores.push({'texto': 'Agrega Nombre al Proyecto'});
-    }
+    res.render('Index', { // Llama al View llamado 'Index'
+        nombrePagina: 'Proyectos',
+        proyectos
+    });
+}
 
-    if(errores.length > 0){
+exports.FormularioProyecto = async (req,res) => {
+    
+    const proyectos = await Proyectos.findAll();
+
+    res.render('NuevoProyecto', { 
+        nombrePagina: 'Nuevo Proyecto',
+        proyectos
+    });
+}
+
+exports.NuevoProyecto = async (req,res) => {
+    const proyectos = await Proyectos.findAll();
+    var errores = [];
+
+    try { 
+        //Validar que tengamos algo en el input.
+        const nombre = req.body.nombre;
+        
+        var errores = [];
+
+        if(!nombre || nombre === ''){
+            errores.push({'texto': 'Agrega Nombre al Proyecto'});
+        }
+
+        if(errores.length > 0){
+            res.render('nuevoProyecto', {
+                nombrePagina: 'Nuevo Proyecto', 
+                errores,
+                proyectos
+            });
+        } else {
+            const proyecto = await Proyectos.create({ nombre });
+            res.redirect('/');
+        }
+    } catch (ex) {
+        
+        if(ex){
+            errores.push({'texto': 'Error al agregar el Proyecto'});
+        }
+
+        console.error(ex);
+
         res.render('nuevoProyecto', {
             nombrePagina: 'Nuevo Proyecto', 
-            errores
+            errores,
+            proyectos
         });
-    } else {
-        //No hay errores
-        //Insertar en la BD
     }
+}
 
+exports.ProyectoPorUrl = async (req,res) => {
+    const proyectos = await Proyectos.findAll();
+    const proyecto = await Proyectos.findOne({
+        where: {
+            url: req.params.url
+        }
+    });
+
+    if(!proyecto) return next();
+
+    res.render('tareas', {
+        nombrePagina: 'Tareas del Proyecto',
+        proyecto,
+        proyectos
+    })
 }
