2f9cc8610ceefa8e3ccd67fc74a222019fca2226
Juan Cruz <juanbonino97@gmail.com>
2020-07-04 21:14:05 -0300

    Fix autoincrement + add hook

--- Models/Proyectos.js
+++ Models/Proyectos.js
@@ -1,13 +1,25 @@
 const Sequelize = require('sequelize');
 const Db = require('../Config/Db');
+const slug = require('slug');
+const shortid = require('shortid');
+
 const Proyectos = Db.define('proyectos', {
     id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoincrement: true
+        autoIncrement: true
     },
     nombre: Sequelize.STRING,
     url: Sequelize.STRING
+}, {
+    hooks: {
+        //Se ejecuta antes de insertar en la Base de Datos.
+        //https://sequelize.org/v4/manual/tutorial/hooks.html
+        beforeCreate(proyecto){   
+            const url = slug(proyecto.nombre).toLocaleLowerCase();
+            proyecto.url = `${url}-${shortid.generate()}`;
+        }
+    }
 });
 
 module.exports = Proyectos;
