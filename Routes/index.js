9a75838640ebfffb714c1054cbf604a2bbb807a2
bonino97 <juanbonino97@gmail.com>
2020-07-10 17:35:28 -0300

    Add TareasRouting

--- Routes/index.js
+++ Routes/index.js
@@ -5,15 +5,16 @@ const router = express.Router();
 //https://express-validator.github.io/docs/
 const { body } = require('express-validator');
 
-//Importar Controlador
+//Importar Controladores
 const ProyectosController = require('../Controllers/ProyectosController');
+const TareasController = require('../Controllers/TareasController')
+
+//Importar Modelos
 const Proyectos = require('../Models/Proyectos');
-
-
 
 module.exports = ()=> {
 
-    /* Rutas */
+    /* Rutas ProyectoController*/
 
     //GET
     router.get('/', ProyectosController.Home);
@@ -36,6 +37,20 @@ module.exports = ()=> {
         body('nombre').not().isEmpty().trim().escape(),
         ProyectosController.ActualizarProyecto
     );
+
+    //DELETE
+    router.delete('/proyectos/:url', ProyectosController.EliminarProyecto); 
+
+    /* Rutas TareasController*/
+
+    //POST
+    router.post('/proyectos/:url', TareasController.AgregarTarea);
+
+    //PATCH
+    router.patch('/tareas/:id', TareasController.ModificarEstadoTarea);
+ 
+    //DELETE
+    router.delete('/tareas/:id', TareasController.EliminarTarea);
 
     return router;
 }
