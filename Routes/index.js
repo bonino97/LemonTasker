08a7ae32f686382825a90eedf22fa89ed28ff89a
bonino97 <juanbonino97@gmail.com>
2020-07-14 14:51:02 -0300

    Update Routes

--- Routes/index.js
+++ Routes/index.js
@@ -7,7 +7,9 @@ const { body } = require('express-validator');
 
 //Importar Controladores
 const ProyectosController = require('../Controllers/ProyectosController');
-const TareasController = require('../Controllers/TareasController')
+const TareasController = require('../Controllers/TareasController');
+const UsuariosController = require('../Controllers/UsuariosController');
+const AuthController = require('../Controllers/AuthController');
 
 //Importar Modelos
 const Proyectos = require('../Models/Proyectos');
@@ -17,40 +19,56 @@ module.exports = ()=> {
     /* Rutas ProyectoController*/
 
     //GET
-    router.get('/', ProyectosController.Home);
-    router.get('/nuevo-proyecto', ProyectosController.FormularioProyecto);
+    router.get('/', AuthController.UsuarioAutenticado, ProyectosController.Home);
+    router.get('/nuevo-proyecto', AuthController.UsuarioAutenticado, ProyectosController.FormularioProyecto);
     // Listar Proyecto
-    router.get('/proyectos/:url', ProyectosController.ProyectoPorUrl);
+    router.get('/proyectos/:url', AuthController.UsuarioAutenticado, ProyectosController.ProyectoPorUrl);
     // Editar Proyecto
-    router.get('/proyecto/editar/:id', ProyectosController.EditarProyecto);
+    router.get('/proyecto/editar/:id', AuthController.UsuarioAutenticado, ProyectosController.EditarProyecto);
 
 
     //POST
     router.post(
-        '/nuevo-proyecto', 
+        '/nuevo-proyecto',
+        AuthController.UsuarioAutenticado,
         body('nombre').not().isEmpty().trim().escape(),
         ProyectosController.NuevoProyecto
     );
 
     router.post(
-        '/nuevo-proyecto/:id', 
+        '/nuevo-proyecto/:id',
+        AuthController.UsuarioAutenticado,
         body('nombre').not().isEmpty().trim().escape(),
         ProyectosController.ActualizarProyecto
     );
 
     //DELETE
-    router.delete('/proyectos/:url', ProyectosController.EliminarProyecto); 
+    router.delete('/proyectos/:url', AuthController.UsuarioAutenticado, ProyectosController.EliminarProyecto); 
 
     /* Rutas TareasController*/
 
     //POST
-    router.post('/proyectos/:url', TareasController.AgregarTarea);
+    router.post('/proyectos/:url', AuthController.UsuarioAutenticado, TareasController.AgregarTarea);
 
     //PATCH
-    router.patch('/tareas/:id', TareasController.ModificarEstadoTarea);
+    router.patch('/tareas/:id', AuthController.UsuarioAutenticado, TareasController.ModificarEstadoTarea);
  
     //DELETE
-    router.delete('/tareas/:id', TareasController.EliminarTarea);
+    router.delete('/tareas/:id', AuthController.UsuarioAutenticado, TareasController.EliminarTarea);
+
+    /* Rutas Usuarios Controller*/
+    //GET
+    router.get('/crear-cuenta', UsuariosController.PaginaRegistro);
+    //GET
+    router.get('/iniciar-sesion', UsuariosController.PaginaLogin);
+
+    //POST
+    router.post('/crear-cuenta', UsuariosController.CrearCuenta);
+    
+    /* Rutas Auth Controller*/
+    router.post('/iniciar-sesion', AuthController.AutenticarUsuario);
+
+    router.get('/cerrar-sesion', AuthController.CerrarSesion);
 
     return router;
 }
