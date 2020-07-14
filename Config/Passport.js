e87b404cf8d5fa154ab91a1ea080d10940de21ee
bonino97 <juanbonino97@gmail.com>
2020-07-14 14:50:02 -0300

    Add Passport

---
+++ Config/Passport.js
@@ -0,0 +1,52 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+//Referencia al modelo de autenticacion
+const Usuarios = require('../Models/Usuarios');
+
+//LocalStrategy - Login con Credenciales propios (Usuario y Password)
+passport.use(
+    new LocalStrategy(
+        //Por default passport espera un Usuario y Password
+        { 
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async(email, password, done) => {
+            try {
+                console.log(email, password);
+                const usuario = await Usuarios.findOne({
+                    where: { email: email} 
+                });
+                
+                //El usuario existe pero password incorrecto.
+                if(!usuario.VerificarPassword(password)){
+                    return done(null, false, {
+                        message: 'Password incorrecto.'
+                    })
+                }
+                //Email existe password correcto
+                return done(null, usuario);
+            } catch(error) { 
+                //Ese usuario no existe.
+                return done(null, false, {
+                    message: 'Email o password invalido.'
+                })
+            }
+        }
+    )
+)
+
+//Serializar el usuario
+passport.serializeUser((usuario, callback) => {
+    callback(null, usuario);
+});
+
+//Deserializar el usuario
+passport.deserializeUser((usuario, callback) => {
+    callback(null, usuario);
+});
+
+
+//Exportar usuario
+module.exports = passport;
