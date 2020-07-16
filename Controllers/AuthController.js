52c4c5a5f5c69f1ee8c61e69fe0cf30079a5a3ca
bonino97 <juanbonino97@gmail.com>
2020-07-16 00:03:14 -0300

    Add Passwort Reset Functionality

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,4 +1,9 @@
 const passport = require('passport');
+const Usuarios = require('../Models/Usuarios');
+const crypto = require('crypto');
+const { Op } = require("sequelize");
+const bcrypt = require('bcrypt-nodejs');
+const enviarEmail = require('../Handlers/Email');
 
 
 //Passport tiene muchas estrategias, la que vamos a usar es local 
@@ -26,4 +31,88 @@ exports.CerrarSesion = (req,res) => {
     req.session.destroy(() => {
         res.redirect('/iniciar-sesion')
     }) 
+}
+
+exports.EnviarToken = async (req,res) => { //Genera un Token si el usuario es valido.
+    //Verificar que el usuario exista
+    const usuario = await Usuarios.findOne({
+        where: {
+            email : req.body.email
+        }
+    })
+
+    //Si no existe el usuario
+    if(!usuario){
+        req.flash('error', 'No existe esa cuenta.');
+        res.render('ReestablecerContraseña', {
+            nombrePagina: 'Reestablecer Contraseña',
+            mensajes: req.flash()
+        })
+    }
+    //Usuario Existe
+    usuario.token = crypto.randomBytes(20).toString('hex');
+    //Expiracion
+    usuario.expiracion = Date.now()+3600000;
+
+    //Guardo en la Base de Datos
+    await usuario.save();
+
+    //Url de reset
+    const resetUrl = `http://${req.headers.host}/reestablecer-password/${usuario.token}`;
+
+    //Envia correo con el Token
+    await enviarEmail.EnviarCorreo({
+        usuario,
+        subject: 'Password Reset',
+        resetUrl,
+        archivo: 'ResetPassword'
+    });
+
+    req.flash('correcto', 'Verifique su Correo.')
+    res.redirect('/iniciar-sesion')
+}
+
+exports.ValidarToken = async (req,res) => {
+    const usuario = await Usuarios.findOne({
+        where: {
+            token: req.params.token
+        }
+    }) 
+
+    if(!usuario) {
+        req.flash('error', 'Token invalido');
+        res.redirect('/reestablecer-password')
+    }
+
+    //Formulario para generar el Password
+    res.render('NuevaContraseña', {
+        nombrePagina: 'Nueva Contraseña'
+    })
+}
+
+//Cambia password por uno nuevo.
+exports.ActualizarPassword = async (req,res) => {
+    const usuario = await Usuarios.findOne({
+        where: {
+            token: req.params.token,
+            expiracion: {
+                [Op.gte]: Date.now()
+            }
+        }
+    }) 
+    //Verificamos si el usuario existe
+    if(!usuario) {
+        req.flash('error', 'Token invalido');
+        res.redirect('/reestablecer-password')
+    }
+
+    usuario.token = null;
+    usuario.expiracion = null;
+    //Hashear el nuevo password
+    usuario.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+
+    await usuario.save();
+
+    req.flash('correcto', 'Tu password se ha modificado correctamente.');
+    res.redirect('/iniciar-sesion');
 }
